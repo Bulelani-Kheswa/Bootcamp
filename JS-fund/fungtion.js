2a2f43701da037a3c6807f86f69d4a5ea26a5e09
Bulelani Kheswa <bpkheswa@mail.com>
2023-03-29 16:03:16 +0200

    js_logics

---
+++ JS-fund/fungtion.js
@@ -0,0 +1,43 @@
+//var a=1;
+//var a=10
+/*let a=5;
+let a=10*/
+
+/*let a= 10;
+let b= 70;
+let c= 10
+let d= 20
+
+if(a==c&&c==d){
+
+
+    console.log ("true");
+}else{
+console.log(false);
+}
+*/
+
+//console.log(c);
+/*let a = -10;
+
+if ( a>10 ){
+    console.log("Greater than a decade");
+} else if (a == 10){
+    console.log("it is decade")
+} else if (a < 10 && a >= 0 ){
+    console.log("it is lesser than a decade")
+} else {
+    console.log("we cannot have negative years!")
+}
+*/
+
+let a = 100;
+let b = 50;
+let c = 25;
+
+if ( a>b || a>c){
+    console.log(true);
+}   else if (a<b && a<c){
+    console.log(false);
+}
+
