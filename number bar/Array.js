77898865e9d2abe1d04e7612d31f8e6d4306b9b8
Bulelani Kheswa <bpkheswa@mail.com>
2023-04-03 16:15:29 +0200

    function

--- number bar/Array.js
+++ number bar/Array.js
@@ -20,7 +20,7 @@ console.log (array)
 
 */
 
-let numbers = [20,311,12,11,13,15,9,4,0,10]
+/*let numbers = [20,311,12,11,13,15,9,4,0,10]
 
 let even = [];
 let odd = [];
@@ -39,3 +39,112 @@ for(let i = 0; i < numbers.length ; i++){
 console.log(even)
 
 console.log(odd)
+*/
+
+/*const points [-3,-11,12,4,6,7,20]
+ProcessingInstruction.sort(function(a,b){return a-b});
+let lowest = points[0]
+
+
+console.log(points)*/
+
+// let letters = ["e","b","j","d","a","f","g","c"]
+
+// letters.sort()
+
+/*-------------------Accending order numbers----------------------
+
+let numbers = [-20,311,12,-11,13,15,9]
+numbers.sort((a,b)=> a-b)
+
+console.log(numbers)
+*/
+
+//---------------------------------String-------
+/* const array = "bulelani"
+const reversedArray = []
+
+for(let i = array.legth-1;i>=0;i--){
+    const valueAtIndex = array[i]
+
+    reversedArray.push(charAtIndex)
+}
+
+console.log(reversedArray)
+*/
+
+// function maxAndMin (numbers){
+ /*   let obj = {}
+    numbers.sort((a,b)=>a-b)
+    
+    obj["minNumber"] = numbers[0]
+    obj["maxNumber"] = numbers[numbers.length-1]
+
+     return obj
+    let firstApp = maxAndMin([-20,311,12,-11,13,15,9])
+    console (firstApp.minNumber)
+
+    //console.log(obj)
+}
+
+maxAndMin ([])
+let firstApp = maxAndMin
+let firstApp = maxAndMin ([-20,311,12,-11,13,15,9])
+console.log(firstApp)
+*/
+
+/*---------------------------reversed text-------------
+function strReversed (text) {
+    let reversed = "";
+    //for (let i = 0 ; 1 < text.length; i ++){
+    for (let i = 0 ;i < 10 ; i++){
+    reversed = text[i] + reversed;
+    
+    }
+    console.log(reversed);
+}
+
+strReversed()
+
+strReversed ("Javascript is super easy");
+
+strReversed ("I am testing");
+*/
+
+
+// -------------------------------------salary calculations
+/*
+function salary(1st,2nd,3rd)
+        let salary = "1st"
+        for(let 1st i*31)*/
+
+
+/*        function calSalary(hr,r,d){
+            let calSalary = hr*r*d
+            //return hr*h*d
+            console.log(calSalary)
+        }
+
+
+calSalary (32,7,20)
+  */     
+
+  /*function emailGen(fname="Jon", iname = "Don"){
+    let email=fname + "." + iname + "@gsb.co.za"
+    return email.toLowerCase()            //-----lower cases
+
+  }
+
+ // console.log(emailGen())
+  console.log(emailGen("Bulelani","Kheswa"))
+  */
+
+  let num1= [1,3,5,7]
+  let num2= [0,2,4,6]
+  let num3= [...num1,...num2]
+  let num4= num1.concat(num2)
+
+  let num1.sort((a,b)=> a-b)
+
+  console.log("This is num3", num3)
+ console.log("This is num2", num4)
