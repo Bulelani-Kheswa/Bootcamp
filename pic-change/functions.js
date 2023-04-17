46557c16e943fd60767df6051d5ecb2702a94c68
Bulelani Kheswa <bpkheswa@mail.com>
2023-04-17 16:18:30 +0200

    monday update

--- pic-change/functions.js
+++ pic-change/functions.js
@@ -1,7 +1,13 @@
 
+let images;
+if(sessionStorage.getItem("pics")){images=JSON.parse(sessionStorage.getItem("pics"))}
+else{images = [ "one.jpg", "two.jpeg", "three.jpg"]}
 
-let images = [ "one.jpg", "two.jpeg", "three.jpg"]
-img = document.getElementById("next")
+
+// let images = [ "one.jpg", "two.jpeg", "three.jpg"]
+// img = document.getElementById("next")
+
+
 let i = 0;
 
 img.src = images[i];
@@ -11,8 +17,21 @@ function AddSub(){
 let pic = document.getElementById("submit").value
 images.push(pic);
 
-
 document.getElementById("submit").value = '';
+
+sessionStorage.setItem('pics',JSON.stringify(images))
+let B = sessionStorage.getItem(JSON.parse('pics'))
+
+
+// function AddSub(){
+//     let pic = document.getElementById("submit").value
+//     images.push(pic);
+    
+//     document.getElementById("submit").value = '';
+    
+//     sessionStorage.setItem('pics',JSON.stringify(images))
+//     let B = sessionStorage.getItem(JSON.parse('pics'))
+
 
 
 }
@@ -33,3 +52,23 @@ function prev() {
         {i = images.length - 1}
         img.src = images[i];
     }
+
+
+
+
+//     function next() {
+
+//    i++;
+
+//     if(i >=images.length)
+//     {i = 0}
+//     img.src = images[i];
+// }
+
+// function prev() {
+   
+//        i--;
+//         if(i < 0)
+//         {i = images.length - 1}
+//         img.src = images[i];
+//     }
